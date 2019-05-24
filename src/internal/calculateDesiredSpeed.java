adc9bd1112b3223ac421b7d4448bbba0109ecc9e
danitoth4 <tdaniel9854@gmail.com>
2019-05-24 14:16:19 +0200

    internal action added

---
+++ src/internal/calculateDesiredSpeed.java
@@ -0,0 +1,21 @@
+// Internal action code for project ier_hf_project.mas2j
+
+package internal;
+
+import jason.*;
+import jason.asSemantics.*;
+import jason.asSyntax.*;
+
+public class calculateDesiredSpeed extends DefaultInternalAction {
+
+    @Override
+    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+        // execute the internal action
+        ts.getAg().getLogger().info("executing internal action 'internal.calculateDesiredSpeed'");
+		
+		double t = (double)args[0] / (double)args[1];
+		int i = (int)Math.floor(t);
+		return i * 5;
+    }
+}
+
