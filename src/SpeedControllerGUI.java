66c467d9f61a27931bb5ea243fc108e017e95e0b
danitoth4 <tdaniel9854@gmail.com>
2019-05-24 11:28:08 +0200

    gui for setting reference speed

---
+++ src/SpeedControllerGUI.java
@@ -0,0 +1,46 @@
+import jason.architecture.*;
+import jason.asSemantics.ActionExec;
+import jason.asSyntax.ASSyntax;
+import jason.asSyntax.Literal;
+import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class SpeedControllerGUI extends AgArch
+{
+	GridLayout grid;
+	JTextField referenceSpeedField;
+	JFrame frame;
+	JButton setSpeedButton;
+	
+	public SpeedControllerGUI()
+	{
+		super();
+		
+		referenceSpeedField = new JTextField();
+		setSpeedButton = new JButton("Set speed!");
+		
+		setSpeedButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try 
+				{
+					Literal goal = ASSyntax.createLiteral("speedChanged", ASSyntax.createNumber(Double.parseDouble(referenceSpeedField.getText())));
+					getTS().getC().addAchvGoal(goal, null);
+				}
+				catch(Exception ex)
+				{
+					System.out.println(ex.getMessage());
+				}
+            }
+        });
+		grid = new GridLayout(2, 1);
+		frame = new JFrame("Speed Controller Monitor");
+		frame.getContentPane().setLayout(grid);
+		frame.add(referenceSpeedField);
+		frame.add(setSpeedButton);
+		frame.setSize(300, 300);
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
+}
